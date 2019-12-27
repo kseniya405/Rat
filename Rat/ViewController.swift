b73fd979ea5d24850d455b21a1c81a4f8a122909
Denis Markov <keynsianets@gmail.com>
2019-12-27 12:13:28 +0200

    Initial Commit

---
+++ Rat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rat
+//
+//  Created by Денис Марков on 27.12.2019.
+//  Copyright © 2019 Kseniia Shkurenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
